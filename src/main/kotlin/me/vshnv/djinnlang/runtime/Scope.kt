7545a3fe7c364eae82417bec611d6af99ffac8e6
Vaishnav <vaishnav.anil@rocketmail.com>
2020-07-03 06:00:04 +0530

    Move Scope in with StateHolder

--- src/main/kotlin/me/vshnv/djinnlang/runtime/Scope.kt
+++
@@ -1,6 +0,0 @@
-package me.vshnv.djinnlang.runtime
-
-data class Scope (val variableMap: Map<String, Variable<*>> = mapOf())
-
-
-data class Variable<T>(val name: String, val value: T) where T: Type
--- src/main/kotlin/me/vshnv/djinnlang/runtime/StateHolder.kt
+++ src/main/kotlin/me/vshnv/djinnlang/runtime/StateHolder.kt
@@ -1,5 +1,7 @@
 package me.vshnv.djinnlang.runtime
 
+import java.lang.Exception
+
 object StateHolder {
     val scopeMap: Map<Int, Scope> = mapOf()
 
@@ -7,5 +9,13 @@ object StateHolder {
         for (i in scope downTo 0) {
             return scopeMap[scope]?.variableMap?.get(name) ?:continue
         }
+        throw Exception("Variable out of scope!")
     }
 }
+
+
+
+data class Scope (val variableMap: Map<String, Variable<*>> = mapOf())
+
+
+data class Variable<T>(val name: String, val value: T) where T: Type
